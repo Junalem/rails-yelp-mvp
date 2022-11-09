acd6d334f03a789d77e3ba434138c517f9a56588
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-09 16:05:11 +0900

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Restaurant.create!(name: "Bistro Mikami", address: "Nakameguro", phone_number: "-", category: "french")
+# Restaurant.create!(name: "Restaurant Unique", address: "Meguro", phone_number: "-", category: "belgian")
+# Restaurant.create!(name: "Locale", address: "Nakano", phone_number: "-", category: "italian")
+# Restaurant.create!(name: "Mugi no Oto", address: "Shibuya", phone_number: "-", category: "japanese")
+# Restaurant.create!(name: "Downtown B’s Indian Kitchen", address: "Shinjuku", phone_number: "-", category: "chinese")
+
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::TYPE.sample
+  )
+end
